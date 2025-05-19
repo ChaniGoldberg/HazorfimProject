f0e9502270ccf4d0d44b9cca91d8e34b613f94d6
rikisabo <ns9760580@gmail.com>
2025-05-19 11:41:50 +0300

    fix nav1 and nav2

--- HazorfimProject/hazorfim/src/components/nav1/Nav1.js
+++ HazorfimProject/hazorfim/src/components/nav1/Nav1.js
@@ -1,70 +1,54 @@
 import React, { useContext } from 'react';
 import '../../styles/Nav1.css';
 import { TiUserAddOutline, TiLocationOutline, TiHeartOutline, TiShoppingCart } from "react-icons/ti";
-import Branches from './Branches';
-import Basket from './Basket';
-import AccountComponent from './AccountComponent ';
-import Favorites from './Favorites';
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 import ThemeContext from '../globalComponants/ThemeContext';
 
 const Nav1 = () => {
+  
   const { theme, toggleTheme } = useContext(ThemeContext);
 
   return (
-    <Router className={theme}> {/* Apply the theme class here */}
-      <div className={`navbar ${theme}`}>
-        <div className="navbar-center">
-          <ul className="nav-links">
-            <li>
-              <Link to="/Basket">
-                <p>סל קניות</p>
-                <TiShoppingCart />
-              </Link>
-            </li>
-            <li>
-              <Link to="/AccountComponent">
-                <p>כניסה</p>
-                <TiUserAddOutline />
-              </Link>
-            </li>
-            <li>
-              <Link to="/Favorites">
-                <p>מועדפים</p>
-                <TiHeartOutline />
-              </Link>
-            </li>
-            <li>
-              <Link to="/Branches">
-                <p>סניפים</p>
-                <TiLocationOutline />
-              </Link>
-            </li>
-          </ul>
-          {/* Toggle Theme Button */}
-
-
-        </div>
-        <div className="navbar-left" id='conect'>
-          <div className="logo">צור קשר:0747809175</div>
-          
-          <button onClick={toggleTheme}>
-            {theme === "dark" ? "  Light  " : "  Dark  "}
-          </button>
-        </div>
- 
-
-     
+    <nav>
+    <div className={`navbar ${theme}`}>
+      <div className="navbar-center">
+        <ul className="nav-links">
+          <li>
+            <Link to="/Basket">
+              <p>סל קניות</p>
+              <TiShoppingCart />
+            </Link>
+          </li>
+          <li>
+            <Link to="/AccountComponent">
+              <p>כניסה</p>
+              <TiUserAddOutline />
+            </Link>
+          </li>
+          <li>
+            <Link to="/Favorites">
+              <p>מועדפים</p>
+              <TiHeartOutline />
+            </Link>
+          </li>
+          <li>
+            <Link to="/Branches">
+              <p>סניפים</p>
+              <TiLocationOutline />
+            </Link>
+          </li>
+        </ul>
       </div>
-
-
-      <Routes>
-        <Route path="/Basket" element={<Basket />} />
-        <Route path="/AccountComponent" element={<AccountComponent />} />
-        <Route path="/Branches" element={<Branches />} />
-        <Route path="/Favorites" element={<Favorites />} />
-      </Routes>
-    </Router>
+      <div className="navbar-left" id='conect'>
+        <div className="logo">צור קשר:0747809175</div>
+        
+        <button onClick={toggleTheme}>
+          {theme === "dark" ? "  Light  " : "  Dark  "}
+        </button>
+      </div>
+    </div>
+    </nav>
   );
 };
+
 export default Nav1;
--- HazorfimProject/hazorfim/src/components/routers/AppRouters.js
+++ HazorfimProject/hazorfim/src/components/routers/AppRouters.js
@@ -5,10 +5,14 @@ import CategoryPage from '../../screens/CategoryPage.js'; // Import your Categor
 import PopupToNav from '../nav2/PopupToNav.js';
 import HomePage from '../../screens/HomePage.js';
 import ProductDetail from '../categoryPage/components/ProductDetail.js';
+import Branches from '../nav1/Branches.js';
+import Basket from '../nav1/Basket.js';
+import AccountComponent from '../nav1/AccountComponent .js'; 
+import Favorites from '../nav1/Favorites.js'; // Import your Favorites component
 //read the links from the links page
 const AppRoutes = () => {
     return (
-        <Routes>
+         <Routes>
             {/* {links.map((link) => (
                 <Route
                     key={link.link}
@@ -17,11 +21,14 @@ const AppRoutes = () => {
                 />
             ))} */}
             
-            <Route path="/categories/:categoryName" element={<CategoryPage/>} />
-            <Route path="/categories/:categoryName/products/:productId" element={<ProductDetail/>} />
+            <Route path="/categories/:categoryName" element={<CategoryPage />} />
+            <Route path="/categories/:categoryName/products/:productId" element={<ProductDetail />} />
             <Route path='/error' element={<div><h1>error:( this page not exists</h1></div>} />
-        
-            <Route path="/" element={<HomePage></HomePage>} /> {/* Fallback route */}
+            <Route path="/" element={<HomePage />} /> {/* Fallback route */}
+            <Route path="/Basket" element={<Basket />} />
+            <Route path="/AccountComponent" element={<AccountComponent />} />
+            <Route path="/Favorites" element={<Favorites />} />
+            <Route path="/Branches" element={<Branches />} />
         </Routes>
     );
 };
